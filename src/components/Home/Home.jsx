1cf6cf38788c432e72f70e857f86ab89c81dcc5c
chayon1001 <baust.cse.200201001@gmail.com>
2024-11-21 19:36:52 +0600

    implemnet the aos package animation on our home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,30 +1,42 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Banner from '../Banner/Banner';
 import TopBrands from '../TopBrands/TopBrands';
 import BrandOnSale from '../BrandOnSale/BrandOnSale';
 import DailyOffers from '../DailyOffers/DailyOffers';
 import BlogLatest from '../BlogLatest/BlogLatest';
 
+
+import 'aos/dist/aos.css';
+import AOS from 'aos';
+
 const Home = () => {
+    useEffect(() => {
+        AOS.init({
+            duration: 1000,
+            easing: 'ease-in-out',
+            once: true,
+        });
+    }, []);
+
     return (
         <div className='mt-3'>
-           <Banner></Banner>
+            <div  data-aos='fade-up'>
+                <Banner></Banner>
 
-           <div>
-                <TopBrands></TopBrands>
+            </div>
 
-
-           </div>
-           <div>
-                <BrandOnSale></BrandOnSale>
-           </div>
-
-           <div>
-                <DailyOffers></DailyOffers>
-           </div>
-           <div>
-                <BlogLatest></BlogLatest>
-           </div>
+            <div data-aos="fade-left">
+                <TopBrands />
+            </div>
+            <div data-aos="fade-right">
+                <BrandOnSale />
+            </div>
+            <div data-aos="zoom-in">
+                <DailyOffers />
+            </div>
+            <div data-aos="fade-up">
+                <BlogLatest />
+            </div>
         </div>
     );
 };

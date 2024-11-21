5c7b90d50c587ce31663e9785cdbd2c2b4f22a88
chayon1001 <baust.cse.200201001@gmail.com>
2024-11-21 13:56:50 +0600

    fixed the error element page

--- src/pages/ErrorElement/ErrorElement.jsx
+++ src/pages/ErrorElement/ErrorElement.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const ErrorElement = () => {
     return (
-        <div>
+        <div className='min-h-screen flex flex-col space-y-4 justify-center items-center'>
             <h2  className='text-3xl font-bold'>Page is not found...!</h2>
+            <Link to='/' className='btn bg-emerald-900 text-white text-xl'>Back to home page</Link>
         </div>
     );
 };
